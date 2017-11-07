b11617ca69134bb4a9266500c9487175ceee3466
Frederick Sandalo <uscbsitric@gmail.com>
2017-11-07 09:47:25 +0800

    Saved Up To Master/Detail Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule }   from '@angular/platform-browser';
+import { NgModule }        from '@angular/core';
+import { FormsModule }     from '@angular/forms'; // <-- NgModel lives here 
+import { AppComponent }    from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+ 
+@NgModule({declarations: [AppComponent,
+                          HeroesComponent,
+                          HeroDetailComponent
+                         ],
+           imports: [BrowserModule,
+                     FormsModule
+                    ],
+           providers: [],
+           bootstrap: [AppComponent]
+          }
+        )
 
-import { AppComponent } from './app.component';
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
 export class AppModule { }
