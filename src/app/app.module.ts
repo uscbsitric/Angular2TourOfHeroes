67129940413b9b4ede58717fe6f3b3652d576314
Frederick Sandalo <uscbsitric@gmail.com>
2017-11-12 02:11:35 +0800

    Created Messages Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,18 @@ import { FormsModule }         from '@angular/forms'; // <-- NgModel lives here
 import { AppComponent }        from './app.component';
 import { HeroesComponent }     from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { HeroService }         from './hero.service'
+import { HeroService }         from './hero.service';
+import { MessagesComponent } from './messages/messages.component';
  
 @NgModule({declarations: [AppComponent,
                           HeroesComponent,
-                          HeroDetailComponent
+                          HeroDetailComponent,
+                          MessagesComponent
                          ],
            imports: [BrowserModule,
                      FormsModule
                     ],
-           providers: [HeroService, /*MessageService*/], // The providers array tells Angular to create a single, 
+           providers: [HeroService, ], // The providers array tells Angular to create a single, 
                                                      // shared instance of HeroService and inject into any class that asks for it.
            bootstrap: [AppComponent]
           }
