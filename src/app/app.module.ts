fa52313af0a024bec3e7bec65ff1cc2a49008747
Frederick Sandalo <frederick.sandalo@zoramteq.com>
2017-11-17 16:29:50 +0800

    In Delete Hero Details from Heroes Component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,10 @@ import { AppComponent }        from './app.component';
 import { HeroesComponent }     from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroService }         from './hero.service';
-import { MessagesComponent } from './messages/messages.component';
-import { MessageService } from './message.service';
-import { AppRoutingModule } from './/app-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { MessagesComponent }   from './messages/messages.component';
+import { MessageService }      from './message.service';
+import { AppRoutingModule }    from './/app-routing.module';
+import { DashboardComponent }  from './dashboard/dashboard.component';
  
 @NgModule({declarations: [AppComponent,
                           HeroesComponent,
