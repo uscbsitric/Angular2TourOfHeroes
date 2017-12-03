cf4809215524e2fddd9fefa4f0679db98c7e298e
Frederick Sandalo <frederick.sandalo@zoramteq.com>
2017-12-04 07:38:10 +0800

    Added A Debug Var

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -21,6 +21,9 @@ export class DashboardComponent implements OnInit
  
   getHeroes(): void
   {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes.slice(1, 5));
+    this.heroService.getHeroes().subscribe(heroes => {this.heroes = heroes.slice(1, 5);
+                                                      let debugVar = 'fdsfsafd';
+                                                     }
+                                          );
   }
 }
